3d0f3e1487333d912bfabeaa0acb331a2e293db6
jooe0824@gmail.com <jooe0824@gmail.com>
2020-08-21 18:34:46 +0900

    3rd practice error

---
+++ 3rdprac/routes/user.js
@@ -0,0 +1,11 @@
+var express = require('express');
+let UserModel = require('../models/user');
+var router = express.Router();
+
+router.post('/signup', async(req,res) => {
+    const { id, name, password, email } = req.body;
+    User.push({id, name, password, email});
+    res.status(200).send(User); //status code 200, json 형식으로 response body 에 입력
+});
+
+module.exports = router;
